13f437e84d681bd4ad86c70f62aae3c21aee190a
Marcos Macedo <marcosmacedoo.dev@gmail.com>
2021-10-02 17:01:53 -0300

    created entity Service

---
+++ src/domain/entities/Service.ts
@@ -0,0 +1,11 @@
+export class Service {
+    id: string;
+    title: string;
+    description: string;
+    budget: number;
+    status: string;
+    created_at: string;
+    updated_at: string;
+    deadline: string;
+    comments: string[];
+}
