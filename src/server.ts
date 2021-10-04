f41abebd7d3b646934a658a263493f02b5be2616
Marcos Macedo <marcosmacedoo.dev@gmail.com>
2021-10-04 19:54:30 -0300

    chore: added cors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express'
+import cors from 'cors'
 import routes from './presentation/routes'
 
 const server = express()
 
+server.use(cors())
 server.use(express.json())
 server.use(routes)
 
