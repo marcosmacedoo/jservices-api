ca42de9ea8d90fc8add64494b21e84c72123793c
Marcos Macedo <marcosmacedoo.dev@gmail.com>
2021-10-09 15:07:23 -0300

    chore: remove comments in file ServiceRoutes.ts

--- src/presentation/routes/ServiceRoutes.ts
+++ src/presentation/routes/ServiceRoutes.ts
@@ -1,9 +1,3 @@
-// GET ('/services') => retorna todos os serviços
-// POST ('/services') => cria um novo serviço
-// GET ('/services/:id') => retorna somente um serviço de acordo com o id
-// PUT ('/services/:id/cancelar') => cancelar o serviço de acordo com o id
-// PUT ('/services/:id/finalizar') => finalizar o serviço de acordo com o id
-// PUT ('/services/:id/comentar') => cria um novo comentário no serviço de acordo com id
 import { Router } from 'express'
 import { ServiceController } from '../controllers/ServiceController'
 
