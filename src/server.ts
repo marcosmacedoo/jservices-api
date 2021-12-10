943718836d28c2836612d7fa77ff0e60266264cf
Marcos Macedo <marcosmacedoo.dev@gmail.com>
2021-12-10 00:02:01 -0300

    refactor: criada a variavel de porta do server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,12 @@ import cors from 'cors'
 import routes from './presentation/routes'
 
 const server = express()
+const port = 3333
 
 server.use(cors())
 server.use(express.json())
 server.use(routes)
 
-server.listen(3000, () => console.log('Server running'))
+server.listen(port, () =>
+    console.log(`Server running in http://localhost:${port}`)
+)
